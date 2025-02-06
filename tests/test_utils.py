ddc893da709c3c25f07f1147ee5490ec11e736f2
Garot Conklin <gmconklin@gmail.com>
2025-02-06 16:36:49 -0500

    garotm - formatting #2.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,13 +5,9 @@ import os
 
 import pytest
 
-from datadog_dashboard_deployer.utils import (
-    format_error,
-    get_version,
-    sanitize_string,
-    setup_logging,
-    validate_environment,
-)
+from datadog_dashboard_deployer.utils import (format_error, get_version,
+                                              sanitize_string, setup_logging,
+                                              validate_environment)
 
 
 def test_setup_logging():
@@ -83,4 +79,3 @@ def test_get_version():
 
     assert isinstance(version, str)
     assert len(version.split(".")) == 3
-    
